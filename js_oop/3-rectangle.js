8a1383c96479098392bcdb40302925c3ebee69c4
nanorisrael <israel.nanor@compozelabs.com>
2023-09-12 16:40:51 +0000

    Add task printer

---
+++ js_oop/3-rectangle.js
@@ -0,0 +1,24 @@
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h
+		}
+	}
+
+	print () {
+		let rows = this.height;
+
+		while (rows > 0) {
+			let columns = this.width;
+			while(columns > 0) {
+				process.stdout.write('X');
+				columns -= 1;
+			}
+			rows -= 1;
+			console.log()
+		} 
+	}
+}
+
+module.exports = Rectangle
