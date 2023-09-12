2f64ee48078991f575a1b7c9744f1a4b61bbfcfb
nanorisrael <israel.nanor@compozelabs.com>
2023-09-12 16:46:58 +0000

    Add rotate and double methods to base

---
+++ js_oop/4-rectangle.js
@@ -0,0 +1,40 @@
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h
+		}
+	}
+
+	print () {
+		let rows = this.height;
+
+		while (rows > 0) {
+			let columns = this.width;
+			while(columns > 0) {
+				process.stdout.write('X');
+				columns -= 1;
+			}
+			rows -= 1;
+			console.log()
+		} 
+	}
+
+	rotate () {
+		let temp = this.width;
+		this.width = this.height;
+		this.height = temp
+
+		return this;
+	}
+
+	double () {
+		
+		this.width = this.width * 2;
+		this.height = this.height * 2;
+
+		return this;
+	}
+}
+
+module.exports = Rectangle
