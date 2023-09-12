d9722da32190c5c3958ee861918533b95249e30f
nanorisrael <israel.nanor@compozelabs.com>
2023-09-12 19:15:09 +0000

    Add print method for square

---
+++ js_oop/6-square.js
@@ -0,0 +1,27 @@
+const Rectangle = require("./5-square.js");
+
+class Square extends Rectangle {
+	constructor(size) {
+		super(size, size)
+	}
+
+	charPrint(c) {
+		if (c) {
+			let rows = this.height;
+
+			while (rows > 0) {
+				let columns = this.width;
+				while(columns > 0) {
+					process.stdout.write('C');
+					columns -= 1;
+				}
+				rows -= 1;
+				console.log()
+			} 
+		} else {
+			super.print()
+		}
+	}
+}
+
+module.exports = Square
