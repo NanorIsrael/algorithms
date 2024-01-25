3a86ee60308af0d2f9195b2bb717d20f8ce255ba
nanorisrael <israel.nanor@compozelabs.com>
2024-01-25 21:33:59 +0000

    Complete log parser

--- scripts/stats.py
+++ scripts/stats.py
@@ -1,4 +1,52 @@
 #!/usr/bin/python3
+'''script to parse logs'''
+import re
+def process_log_line(line):
+	'''Retrieves the necessary components from a logged file'''
+	pattern = re.compile(r'''
+		^
+		(\d+\.\d+\.\d+\.\d+)
+		\s-\s\[([^]]+)\]
+		\s"GET\s/projects/260\sHTTP/1\.1"\s(\d+)\s(\d+)
+		$
+		''', re.VERBOSE)
+	is_valid_line = pattern.match(line)
+	if not is_valid_line:
+		return None
+	# Extract relevant information from the match object
+	ip_address, date, status_code, file_size = is_valid_line.groups()
+	# Convert file_size to an integer
+	file_size = int(file_size)
+	# Return a dictionary with the extracted information
+	return {
+			'ip_address': ip_address,
+			'date': date, 'status_code': status_code,
+			'file_size': file_size
+			}
+
+
+def print_statistics(total_file_size, lines_by_status):
+	 print('File size: {:d}'.format(total_file_size))
+	 for status_code in sorted(lines_by_status):
+		 print('{}: {:d}'.format(status_code, lines_by_status[status_code]))
+
 
 if __name__ == "__main__":
-	[print(line) for line in iter(input, '')]
+	import sys
+	codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+	lines_by_status = {k:0 for k in codes}
+	total_file_size = 0
+	line_count = 0
+	try:
+		for line in sys.stdin:
+			log_entry = process_log_line(line)
+			if log_entry:
+				total_file_size += log_entry['file_size']
+				lines_by_status[log_entry['status_code']] += 1
+				line_count += 1
+				if line_count % 10 == 0:
+					 print_statistics(total_file_size, lines_by_status)
+		print_statistics(total_file_size, lines_by_status)
+	except KeyboardInterrupt:
+		 print_statistics(total_file_size, lines_by_status)
+		 raise
