9c74b2a4e1404829ecbeddd45e47db7a3041d95f
nanorisrael <israel.nanor@compozelabs.com>
2024-01-30 18:06:06 +0000

    Add lrfu cache class

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" LRUCache module
+"""
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+	""" LRUCache defines:
+	  - constants of your caching system
+	  - where your data are stored (in a dictionary)
+	"""
+	RECENTLY_USED = ''
+	def __init__(self):
+		super().__init__()
+	
+	def put(self, key, item):
+		""" Add an item in the cache
+        """
+		if key is None or item is None:
+			return
+		else:
+			size_of_cache = len(self.cache_data.values()) + 1
+			if key in self.cache_data.keys():
+				del self.cache_data[key]
+			elif size_of_cache > BaseCaching.MAX_ITEMS:
+				discarded_key = self.get_least_used(key)
+				print(f"DISCARD: {discarded_key}")
+			self.get_least_used(key)
+			self.cache_data[key] = item
+	
+	def get(self, key):
+		""" Get an item by key
+        """
+		if key is None:
+			return None
+		else:
+			self.get_least_used(key)
+			return self.cache_data.get(key)
+	
+	def get_least_used(self, key):
+		temp = []
+		if len(temp) == BaseCaching.MAX_ITEMS:
+			del temp[0]
+		temp.append(key)
+		return temp[0]
