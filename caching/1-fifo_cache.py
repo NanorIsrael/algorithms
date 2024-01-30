85aa805e3722ae31d7c477c839dd91c3bb7f4975
nanorisrael <israel.nanor@compozelabs.com>
2024-01-30 15:40:38 +0000

    Add fifo cache class

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-""" BasicCache module
+""" FIFOCache module
 """
 from base_caching import BaseCaching
 
-class BasicCache(BaseCaching):
-	""" BasicCache defines:
+class FIFOCache(BaseCaching):
+	""" FIFOCache defines:
 	  - constants of your caching system
 	  - where your data are stored (in a dictionary)
 	"""
 	def __init__(self):
 		super().__init__()
-
+	
 	def put(self, key, item):
 		""" Add an item in the cache
         """
 		if key is None or item is None:
 			return
 		else:
+			size_of_cache = len(self.cache_data.key())
+			if size_of_cache > BaseCaching.MAX_ITEMS:
+				discarded_key = self.cache_data.key()[0]
+				del self.cache_data[discarded_key]
+				print(f"DISCARD: {discarded_key}")
 			self.cache_data[key] = item
 	
 	def get(self, key):
