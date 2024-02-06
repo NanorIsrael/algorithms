5826935837aeaff386affeaa841d63dbc0f35866
nanorisrael <israel.nanor@compozelabs.com>
2024-02-06 17:17:03 +0000

    Add language translate support config

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -4,12 +4,24 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.url_map.strict_slahes = False
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> any:
+    """get language local"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
