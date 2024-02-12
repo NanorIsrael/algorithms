a99ba6b13050b7a8a30570345ba15ef05b1ac52f
nanorisrael <israel.nanor@compozelabs.com>
2024-02-12 13:39:11 +0000

    Multiply two strings

--- scripts/warmup.py
+++ scripts/warmup.py
@@ -1,3 +1,6 @@
+import math
+
+
 def digit_from_right(s, digit_no):
 	if digit_no >= len(s):
 		return 0
@@ -74,3 +77,94 @@ def words_in_a_text(txt):
 
 print(words_in_a_text("The boy is a good programmer hello!."))
 
+# Implement the Caesar cypher
+
+def encrypt(msg: str, key: int) -> str:
+	enc_msg = ''
+	for char in msg:
+		code = ord(char)
+		if code >= 65 and code <= (65 + 26 -1):
+			code -= 65
+			code = (code + key) % 26
+			code += 65
+		enc_msg += chr(code)
+	return enc_msg
+
+print(encrypt('Hello World!', 5))
+
+# bubble sort
+
+def bubble_sort(ar):
+	issortable = True
+	length = len(ar)
+
+	i = 0;
+	while issortable:
+		issortable = False
+		length -= 1
+		for i in range(0, length):
+			temp = ar[i]
+			if temp > ar[i + 1]:
+				ar[i] = ar[i + 1]
+				ar[i + 1] = temp
+				issortable = True
+
+		i += 1
+	return ar
+
+
+print(bubble_sort([3, 5, 2,4, 7, 8]))
+
+# Create a function that will convert a string 
+# containing a binary number into a number
+def binary_to_decimal(sbinary):
+	return int(sbinary, 2)
+
+
+print(binary_to_decimal('111'))
+
+# Find the maximum number in a jagged 
+# array of numbers or array of numbers
+def max_jaggered_array(ar):
+	max_el = 0
+	for el in ar:
+		if isinstance(el, list):
+			el = max_jaggered_array(el)
+		if el > max_el:
+			max_el = el
+	return max_el
+
+print(max_jaggered_array([2, [34], [409, [56, 6]]]))
+
+
+def multDigit(s_num, digit):
+	carry = 0
+	p = ''
+
+	for i in range(len(s_num) - 1, -1 , -1):
+		num_digit = int(s_num[i])
+		prod = digit * num_digit + carry
+		prod_digit = prod % 10
+		carry = math.floor(prod / 10)
+		p = str(prod_digit) + p
+
+	if carry > 0:
+		p = p + carry
+
+	return p
+
+print(multDigit('123', 2))
+
+
+def multi(snum1, snum2):
+	partialResults = []
+	for i in range(len(snum2) - 1, -1, -1):
+		digit = int(snum2[i])
+		partialResult = multDigit(snum1, digit)
+		partialResult +=  "0" * len(partialResults)
+		partialResults.append(partialResult)  
+	sum = ""
+	for num in partialResults:
+		sum = add(sum, num)
+	return sum
+print(multi('123', '111'))
